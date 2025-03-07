38e81c19238c397d99f3ffda795c31bfb19e5c8c
Grzegorz Wisniowski <grzesiekwis200@gmail.com>
2025-03-07 20:18:11 +0100

    dodano koszyk

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/components/quantity_selector.dart';
+import 'package:food_app/models/cart_item.dart';
 import 'package:food_app/models/restaurant.dart';
 
 import 'package:provider/provider.dart';
 
 class MyCartTile extends StatelessWidget {
-  final MyCartTile cartItem;
+  final CartItem cartItem;
 
   const MyCartTile({
     super.key,
@@ -15,19 +17,91 @@ class MyCartTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<Restaurant>(
         builder: (context, restaurant, child) => Container(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.tertiary,
+                  borderRadius: BorderRadius.circular(12)),
               child: Column(
                 children: [
                   Row(
                     children: [
                       //image
 
-                      Image.asset(cartItem.food.imagePath),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.asset(
+                            cartItem.food.imagePath,
+                            height: 80,
+                            width: 80,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
 
                       //name and price
+                      Container(
+                        width: 180,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              cartItem.food.name,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary),
+                            ),
+                            Text(
+                              "\$" + cartItem.food.price.toString(),
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .inversePrimary),
+                            )
+                          ],
+                        ),
+                      ),
 
                       //increment or decrement button
+                      QuantitySelector(
+                          quantity: cartItem.quantity,
+                          food: cartItem.food,
+                          onDecrement: () {
+                            restaurant.removeFromCart(cartItem);
+                          },
+                          onIncrement: () {
+                            restaurant.addToCart(
+                                cartItem.food, cartItem.selectedAddons);
+                          })
                     ],
-                  )
+                  ),
+                  SizedBox(
+                    height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: cartItem.selectedAddons
+                          .map(
+                            (addon) => FilterChip(
+                              label: Row(
+                                children: [
+                                  Text(addon.name + " "),
+                                  Text("\$" + addon.price.toString())
+                                ],
+                              ),
+                              onSelected: (value) {},
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
                 ],
               ),
             ));
